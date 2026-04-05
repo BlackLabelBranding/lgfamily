56fc405ae62f62a32a7877f7e24bbf2a4c52382e
BlackLabelBranding <lance@garzaunlimited.com>
2026-04-04 21:13:29 -0500

    Refactor routing to include AuthGate component

--- apps/web/src/App.jsx
+++ apps/web/src/App.jsx
@@ -1,32 +1,34 @@
-
 import React from 'react';
-import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
-import ScrollToTop from './components/ScrollToTop';
-import DashboardPage from './pages/DashboardPage';
-import CalendarPage from './pages/CalendarPage';
-import TasksPage from './pages/TasksPage';
-import GroceriesPage from './pages/GroceriesPage';
-import FamilyPage from './pages/FamilyPage';
-import PhotosPage from './pages/PhotosPage';
-import VaultPage from './pages/VaultPage';
-import EmergencyPage from './pages/EmergencyPage';
-import SettingsPage from './pages/SettingsPage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ScrollToTop from '@/components/ScrollToTop.jsx';
+import AuthGate from '@/components/auth/AuthGate.jsx';
+import DashboardPage from '@/pages/DashboardPage.jsx';
+import CalendarPage from '@/pages/CalendarPage.jsx';
+import TasksPage from '@/pages/TasksPage.jsx';
+import GroceriesPage from '@/pages/GroceriesPage.jsx';
+import FamilyPage from '@/pages/FamilyPage.jsx';
+import PhotosPage from '@/pages/PhotosPage.jsx';
+import VaultPage from '@/pages/VaultPage.jsx';
+import EmergencyPage from '@/pages/EmergencyPage.jsx';
+import SettingsPage from '@/pages/SettingsPage.jsx';
 
 function App() {
   return (
     <Router>
       <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<DashboardPage />} />
-        <Route path="/calendar" element={<CalendarPage />} />
-        <Route path="/tasks" element={<TasksPage />} />
-        <Route path="/groceries" element={<GroceriesPage />} />
-        <Route path="/family" element={<FamilyPage />} />
-        <Route path="/photos" element={<PhotosPage />} />
-        <Route path="/vault" element={<VaultPage />} />
-        <Route path="/emergency" element={<EmergencyPage />} />
-        <Route path="/settings" element={<SettingsPage />} />
-      </Routes>
+      <AuthGate>
+        <Routes>
+          <Route path="/" element={<DashboardPage />} />
+          <Route path="/calendar" element={<CalendarPage />} />
+          <Route path="/tasks" element={<TasksPage />} />
+          <Route path="/groceries" element={<GroceriesPage />} />
+          <Route path="/family" element={<FamilyPage />} />
+          <Route path="/photos" element={<PhotosPage />} />
+          <Route path="/vault" element={<VaultPage />} />
+          <Route path="/emergency" element={<EmergencyPage />} />
+          <Route path="/settings" element={<SettingsPage />} />
+        </Routes>
+      </AuthGate>
     </Router>
   );
 }
