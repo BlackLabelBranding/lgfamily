8bdb50aaf5556126465c14842bad18ec62c8e2b5
BlackLabelBranding <lance@garzaunlimited.com>
2026-04-04 20:52:30 -0500

    Wrap App component with AuthProvider and StrictMode

--- apps/web/src/main.jsx
+++ apps/web/src/main.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@/App';
 import '@/index.css';
+import { AuthProvider } from '@/lib/AuthProvider.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<App />
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
 );
